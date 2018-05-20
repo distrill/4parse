0b01cfe51c32f65fd81952428fce4240aac49dfd
Brent Hamilton <brent@arthurkn.net>
2018-05-20 12:55:33 -0700

    db stuffs

---
+++ main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type thread struct {
+	id    int    `db:"id"`
+	title string `db:"title"`
+}
+
+type comment struct {
+	id       int
+	imageURL string `db:"image_url"`
+	posterID string `db:"poster_id"`
+	isOP     bool   `db:"is_op"`
+	threadID int    `db:"thread_id"`
+}
+
+type commentData struct {
+	id        int    `db:"id"`
+	dataType  string `db:"data_type"`
+	contents  string `db:"contents"`
+	commentID int    `db:"comment_id"`
+}
+
+type commentReference struct {
+	id              int `db:"id"`
+	parentCommentID int `db:"parent_comment_id"`
+	childCommentID  int `db:"child_comment_id"`
+}
+
+func getAttr(n *html.Node, attr string) string {
+	for _, a := range n.Attr {
+		if a.Key == attr {
+			return a.Val
+		}
+	}
+	return ""
+}
+
+func getDocument(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return html.Parse(strings.NewReader(string(response)))
+}
+
+func getThreadsForPage(url string, threads []string) []string {
+	var err error
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+
+		// grab  thread links
+		if n.Type == html.ElementNode && n.Data == "a" {
+			class := getAttr(n, "class")
+			if class == "replylink" {
+				href := "http://boards.4chan.org/biz/" + getAttr(n, "href")
+				threads = append(threads, href)
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	doc, err := getDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f(doc)
+
+	return threads
+}
+
+func getThreads() []string {
+	threads := getThreadsForPage("http://boards.4chan.org/biz", make([]string, 0))
+
+	for i := 2; i <= 10; i++ {
+		page := strconv.Itoa(i)
+		threads = append(threads, getThreadsForPage("http://boards.4chan.org/biz/"+page, make([]string, 0))...)
+	}
+
+	return threads
+}
+
+func getID(n *html.Node) string {
+	return strings.Replace(getAttr(n, "id"), "m", "", 1)
+}
+
+func getGreentexts(n *html.Node) []string {
+	greentexts := make([]string, 0)
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Data == "span" {
+			greentexts = append(greentexts, c.FirstChild.Data)
+		}
+	}
+	return greentexts
+}
+
+func getReferences(n *html.Node) []string {
+	references := make([]string, 0)
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Data == "a" {
+			references = append(references, strings.Replace(c.FirstChild.Data, ">>", "", -1))
+		}
+	}
+
+	return references
+}
+
+func getContents(n *html.Node) []string {
+	contents := make([]string, 0)
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Data != "a" && c.Data != "span" && c.Data != "br" {
+			contents = append(contents, c.Data)
+		}
+	}
+
+	return contents
+
+}
+
+func doWhatever(threadURL string) {
+	doc, err := getDocument(threadURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+
+		// grab  thread links
+		if n.Type == html.ElementNode && n.Data == "blockquote" {
+			// com := comment{}
+			// fmt.Println(getID(n))
+			// fmt.Println(getReferences(n))
+			// fmt.Println(getGreentexts(n))
+			// fmt.Println(getContents(n))
+			// fmt.Printf("\n\n")
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+}
+
+func getThreadTitle(threadURL string) string {
+	doc, err := getDocument(threadURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := ""
+
+	// for c := n.FirstChild; c != nil; c = c.NextSibling {
+	// 	if c.Data == "span" {
+	// 		greentexts = append(greentexts, c.FirstChild.Data)
+	// 	}
+	// }
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "span" && getAttr(n, "class") == "subject" {
+			// fmt.Println("whatever")
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				// if c.Data == "span" {
+				// greentexts = append(greentexts, c.FirstChild.Data)
+				// }
+				fmt.Println(c.FirstChild.Data)
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+
+	return title
+}
+
+func getThreadID(threadURL string) string {
+	splitThread := strings.Split(threadURL, "/")
+	threadID := splitThread[len(splitThread)-2]
+	return threadID
+}
+
+func main() {
+	// // fmt.Println(getThreads())
+	// threads := getThreads()
+	// // threads := []string{"whatever"}
+	// doWhatever(threads[0])
+	// for _, thread := range threads {
+	// 	// fmt.Println(thread)
+	// 	doWhatever(thread)
+	// }
+	thread := "http://boards.4chan.org/biz/thread/9493753/where-are-those-fools-who-once-proudly-posted"
+	tid := getThreadID(thread)
+	title := getThreadTitle(thread)
+	fmt.Printf("id: %v\ntitle: %v\n", tid, title)
+}
