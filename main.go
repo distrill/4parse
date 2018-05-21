f76f212180980b3bc2b03153a03354f30b85aff0
Brent Hamilton <brent@arthurkn.net>
2018-05-21 11:09:31 -0700

    some shits

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ type thread struct {
 	title string `db:"title"`
 }
 
+type comment struct {
+	id       string `db:"id"`
+	imageURL string `db:"image_url"`
+	posterID string `db:"poster_id"`
+	isOP     bool   `db:"is_op"`
+	threadID string `db:"thread_id"`
+}
+
 var baseURL = "http://boards.4chan.org/biz"
 
 func getDocument(url string) (*html.Node, error) {
@@ -81,6 +89,9 @@ func getThreads() []string {
 	return threads
 }
 
+/*
+ * thread stuffs
+ */
 func getThreadTitle(threadURL string) string {
 	doc, err := getDocument(threadURL)
 	if err != nil {
@@ -113,16 +124,84 @@ func getThreadID(threadURL string) string {
 	return splitThread[len(splitThread)-2]
 }
 
+func getThreadInfo(threadURL string) thread {
+	id := getThreadID(threadURL)
+	title := getThreadTitle(threadURL)
+	return thread{id, title}
+}
+
+/*
+ * comment stuffs
+ */
+func getCommentID(n *html.Node) string {
+	return strings.Replace(getAttr(n, "id"), "m", "", 1)
+}
+
+func getCommentImageURL(n *html.Node) string {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == "div" && getAttr(c, "class") == "file" {
+			return strings.Replace(getAttr(c.FirstChild.FirstChild.NextSibling, "href"), "//", "", 1)
+		}
+	}
+	return ""
+}
+
+// TODO: this is really fragile, this fucky path. There gotta be a better way
+func getPosterID(n *html.Node) string {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == "div" && getAttr(c, "class") == "postInfoM mobile" {
+			return c.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.FirstChild.NextSibling.FirstChild.Data
+		}
+	}
+	return ""
+}
+
+func getCommentsInfo(threadURL string, threadID string) []comment {
+	isOP := true
+	comments := make([]comment, 0)
+
+	doc, err := getDocument(threadURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "blockquote" {
+			cmt := comment{
+				getCommentID(n),
+				getCommentImageURL(n.Parent),
+				getPosterID(n.Parent),
+				isOP,
+				threadID,
+			}
+			fmt.Println(cmt)
+			comments = append(comments, cmt)
+			isOP = false
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return comments
+}
+
 func main() {
 	ts := getThreads()
+	// ts := []string{"http://boards.4chan.org/biz/thread/9537535/im-gonna-make-cuff-links-cause-of-some-faggot-i"}
 	threads := make(map[string]bool)
 	for _, t := range ts {
-		tid := getThreadID(t)
-		title := getThreadTitle(t)
-		td := thread{tid, title}
-		if threads[tid] == false {
+		td := getThreadInfo(t)
+		if threads[td.id] == false && td.id != "904256" && td.id != "4884770" {
+			fmt.Println("THREAD:")
 			fmt.Println(td)
-			threads[tid] = true
+			threads[td.id] = true
+			comments := getCommentsInfo(t, td.id)
+			fmt.Println("COMMENTS:")
+			fmt.Println(comments)
+			fmt.Print("\n\n\n")
 		}
 	}
 }
